a76291a3539151555103e107e27badba7f9ba6f1
vinicius <vigusmao@gmail.com>
2020-09-04 09:53:34 -0300

    Novos testes (senha, fundos)

--- Siguinha/src/AlunoTest.java
+++ Siguinha/src/AlunoTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -5,9 +6,27 @@ import static org.junit.Assert.*;
 
 public class AlunoTest {
 
+    private Aluno aluno;
+    private Disciplina disciplina1;
+    private Disciplina disciplina2;
+    private Disciplina disciplina3;
+
+    @Before
+    public void setUp() {
+        aluno = new Aluno(12345, 2018, "Aluno Teste");
+
+        disciplina1 = new Disciplina("MAB001", "Disciplina 1");
+        disciplina1.setCreditos(4);
+
+        disciplina2 = new Disciplina("MAB002", "Disciplina 2");
+        disciplina2.setCreditos(6);
+
+        disciplina3 = new Disciplina("MAJ003", "Disciplina 3");
+        disciplina3.setCreditos(6);
+    }
+
     @Test
     public void getDreTest() {
-       Aluno aluno = new Aluno(12345, 2018, "Aluno Teste");
        assertEquals(12345, aluno.getDre());
     }
 
@@ -19,8 +38,6 @@ public class AlunoTest {
 
     @Test
     public void getConclusaoDiscipinaTest() {
-        Aluno aluno = new Aluno(12345, 2018, "Aluno Teste");
-
         ItemDeHistorico[] resultadoObtido = aluno.getDisciplinasCursadas();
         assertNotNull(resultadoObtido);
         for (int i = 0; i < resultadoObtido.length; i++) {
@@ -29,8 +46,6 @@ public class AlunoTest {
         assertEquals(0, aluno.getQuantDisciplinasCursadas());
 
         // o aluno vai cursar a primeira disciplina
-        Disciplina disciplina1 = new Disciplina("MAB001", "Disciplina 1");
-        disciplina1.setCreditos(4);
         aluno.registrarConclusaoDisciplina(disciplina1, 6.5f, 2019, 2);
 
         resultadoObtido = aluno.getDisciplinasCursadas();
@@ -38,30 +53,17 @@ public class AlunoTest {
         assertEquals("MAB001", primeiroItem.getDisciplina().getCodigo());
         assertEquals(2019, primeiroItem.getAno());
         assertEquals(2, primeiroItem.getSemestre());
-        assertEquals(1, aluno.getQuantDisciplinasCursadas());
-        assertEquals(6.5f, aluno.getCra(), 0);  // o terceiro parâmetro é a maior diferença aceitável
-        // ou...
-        //assertTrue(aluno.getCra() == 6.5f);
-        assertEquals(4, aluno.getCreditosAcumulados());
+        verificarAtualizacaoCreditos(1, 6.5f, 4);
 
-        // o aluno vai cursar a segunda disciplina
-        Disciplina disciplina2 = new Disciplina("MAB002", "Disciplina 2");
-        disciplina2.setCreditos(6);
         aluno.registrarConclusaoDisciplina(disciplina2, 8, 2020, 1);
-
+        // verificar todas as disciplinas do histórico até aqui
         resultadoObtido = aluno.getDisciplinasCursadas();
         assertEquals("MAB001", primeiroItem.getDisciplina().getCodigo());
         ItemDeHistorico segundoItem = resultadoObtido[1];
         assertEquals("MAB002", segundoItem.getDisciplina().getCodigo());
-        assertEquals(2, aluno.getQuantDisciplinasCursadas());
-        assertEquals(7.4f, aluno.getCra(), 0);  // o terceiro parâmetro é a maior diferença aceitável
-        // ou...
-        //assertTrue(aluno.getCra() == 6.5f);
-        assertEquals(10, aluno.getCreditosAcumulados());
+        verificarAtualizacaoCreditos(2, 7.4f, 10);
 
         // o aluno vai cursar a terceira disciplina
-        Disciplina disciplina3 = new Disciplina("MAJ003", "Disciplina 3");
-        disciplina3.setCreditos(6);
         aluno.registrarConclusaoDisciplina(disciplina3, 10, 2020, 1);
 
         resultadoObtido = aluno.getDisciplinasCursadas();
@@ -69,32 +71,29 @@ public class AlunoTest {
         assertEquals("MAB002", segundoItem.getDisciplina().getCodigo());
         ItemDeHistorico terceiroItem = resultadoObtido[2];
         assertEquals("MAJ003", terceiroItem.getDisciplina().getCodigo());
-        assertEquals(3, aluno.getQuantDisciplinasCursadas());
-        assertEquals(16, aluno.getCreditosAcumulados());
+        verificarAtualizacaoCreditos(3, 8.375f, 16);
     }
 
     @Test
     public void retornarHistoricoTest() {
-        Aluno aluno = new Aluno(12345, 2018, "Aluno Teste");
-
-        Disciplina disciplina1 = new Disciplina("MAB001", "Disciplina 1");
-        disciplina1.setCreditos(4);
         aluno.registrarConclusaoDisciplina(disciplina1, 6.5f, 2019, 2);
-
-        Disciplina disciplina2 = new Disciplina("MAB002", "Disciplina 2");
-        disciplina2.setCreditos(6);
         aluno.registrarConclusaoDisciplina(disciplina2, 8, 2020, 1);
-
-        Disciplina disciplina3 = new Disciplina("MAB003", "Disciplina 3");
-        disciplina3.setCreditos(6);
         aluno.registrarConclusaoDisciplina(disciplina3, 10, 2020, 2);
 
         String historicoRetornado = aluno.retornarHistoricoAsString();
         String historicoEsperado =
                 "MAB001 - média 6.5 - 4 créditos - 2019.2\n" +
                 "MAB002 - média 8.0 - 6 créditos - 2020.1\n" +
-                "MAB003 - média 10.0 - 6 créditos - 2020.2";
+                "MAJ003 - média 10.0 - 6 créditos - 2020.2";
         assertEquals(historicoEsperado, historicoRetornado);
+    }
+
+    private void verificarAtualizacaoCreditos(int quantDiscipinasEsperado,
+                                              float craEsperado,
+                                              int creditosAcumuladosEsperado) {
+        assertEquals(quantDiscipinasEsperado, aluno.getQuantDisciplinasCursadas());
+        assertEquals(craEsperado, aluno.getCra(), 0);  // o terceiro parâmetro é a maior diferença aceitável
+        assertEquals(creditosAcumuladosEsperado, aluno.getCreditosAcumulados());
     }
 
 }
---
+++ SistemaBancario/src/test/BancoTest.java
@@ -0,0 +1,111 @@
+package test;
+
+import main.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BancoTest {
+
+    private static final int FLOAT_DELTA = 0;
+
+    private Banco banco;
+    private Agencia agencia;
+    private Correntista joao;
+    private Correntista maria;
+    private Gerente alice;
+    private Gerente carlos;
+
+    Conta conta1;
+    Conta conta2;
+
+    int senhaConta1;
+    int senhaConta2;
+
+
+    @Before
+    public void setUp() {
+        // crio o banco
+        banco = new Banco();
+
+        // adiciono gerentes
+        alice = banco.adicionarGerente("Alice");
+        carlos = banco.adicionarGerente("Carlos");
+
+        // adiciono uma agência
+        agencia = banco.adicionarAgencia(11111, "Agência Teste 1");
+        agencia.setGerenteGeral(alice);
+
+        // adiciono correntistas
+        senhaConta1 = 1234;
+        joao = banco.adicionarCorrentista("João", senhaConta1);
+        senhaConta2 = 5678;
+        maria = banco.adicionarCorrentista("Maria", senhaConta2);
+
+        // adiciono contas
+        conta1 = banco.criarConta(agencia, joao);
+        conta2 = banco.criarConta(agencia, maria);
+    }
+
+    @Test
+    public void testarCriacaoAgencia() {
+        Assert.assertEquals(1, banco.getQuantAgencias());  // a agência criada no setUp
+        Agencia novaAgencia = banco.adicionarAgencia(44444, "Nova Agência");
+        assertNotNull("O banco deve retornar a agência recém-criada", novaAgencia);
+        Assert.assertEquals(2, banco.getQuantAgencias());  // a agência criada no setUp
+    }
+
+    @Test
+    public void testarCriacaoConta() {
+        Assert.assertEquals("O saldo inicial de uma conta deve ser zero",
+                0, conta1.getSaldo(), 0);
+        Assert.assertEquals("O gerente default de uma conta nova é o gerente geral de sua agência",
+                agencia.getGerenteGeral(), conta1.getGerente());
+        Assert.assertEquals("A conta deve retornar corretamente seu dono", joao, conta1.getCorrentista());
+    }
+
+    @Test
+    public void testarOperacoesBancarias() {
+        conta1.receberDepositoEmDinheiro(300);
+        Assert.assertEquals("O saldo deve refletir depósitos recebidos",
+                300, conta1.getSaldo(), FLOAT_DELTA);
+
+        conta1.sacar(10, senhaConta1);
+        Assert.assertEquals("O saldo da conta de origem deve diminuir após um saque",
+                290, conta1.getSaldo(), FLOAT_DELTA);
+
+        conta1.sacar(1000, senhaConta1);
+        Assert.assertEquals("Não deve ser possível sacar se não houver fundos suficientes (considerando o limite)",
+                290, conta1.getSaldo(), FLOAT_DELTA);
+
+        conta1.efetuarTransferencia(conta2, 250);
+        Assert.assertEquals("O saldo da conta de origem deve diminuir após uma transferência",
+                40, conta1.getSaldo(), FLOAT_DELTA);
+        Assert.assertEquals("O saldo da conta de destino deve aumentar após uma transferência",
+                250, conta2.getSaldo(), FLOAT_DELTA);
+
+        conta1.sacar(130, senhaConta1);
+        Assert.assertEquals("Deve ser possível sacar até o limite de " + Conta.LIMITE, -90,
+                conta1.getSaldo(), FLOAT_DELTA);
+    }
+
+    @Test
+    public void testarSaqueComSenhaIncorreta() {
+        conta1.sacar(10, 3333333);
+        assertEquals("Saques não devem ser permitidos se a senha estiver incorreta",
+                0, conta1.getSaldo(), FLOAT_DELTA);
+
+        conta1.sacar(10, senhaConta1);
+        assertEquals("Saques devem ser permitidos se a senha estiver correta",
+                -10, conta1.getSaldo(), FLOAT_DELTA);
+    }
+
+    @Test
+    public void testarTransferenciaSemFundos() {
+        conta1.efetuarTransferencia(conta2, 500);
+        assertEquals("Não deve ser possível sacar se não houver fundos suficientes (considerando o limite)",
+                0, conta1.getSaldo(), FLOAT_DELTA);
+    }
+}
