c64fdab0d348fcc48a0adb9f2d9b190ac7d6e008
vinicius <vigusmao@gmail.com>
2020-09-11 07:58:27 -0300

    LAB2 - Fracao

---
+++ Fracao/src/Fracao.java
@@ -0,0 +1,31 @@
+public class Fracao {
+
+    /**
+     * Construtor.
+     * O sinal da fração é passado no parâmetro específico.
+     *
+     * @param numerador O numerador (inteiro não-negativo)
+     * @param denominador O denominador (inteiro positivo)
+     * @param positiva se true, a fração será positiva; caso contrário, será negativa
+     */
+    public Fracao(int numerador, int denominador, boolean positiva) {
+        // ToDo IMPLEMENT ME!!!
+    }
+
+    /**
+     * @return um double com o valor numérico desta fração
+     */
+    public double getValorNumerico() {
+        return 0;
+    }
+
+    /**
+     * Retorna uma fração que é equivalente a esta fração (this),
+     * e que é irredutível (numerador e denominador primos entre si)
+     *
+     * @return uma fração irredutível equivalente a esta
+     */
+    public Fracao getFracaoIrredutivel() {
+        return null;
+    }
+}
